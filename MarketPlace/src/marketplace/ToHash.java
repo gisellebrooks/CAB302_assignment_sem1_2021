36494f49f4a196f64340ca0b0d191f46550462c4
JackGL <jackgl@bigpond.com>
2021-04-25 10:52:07 +1000

    Added javadoc to ToHash class

--- MarketPlace/src/marketplace/ToHash.java
+++ MarketPlace/src/marketplace/ToHash.java
@@ -5,11 +5,17 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/** for ecyrpting (SHA-256 + salted) a message (password)
+ @author Jack Gate-Leven
+ */
 public class ToHash {
 
     private static final String salt = "4@#ndssa213";
 
-    // REFERENCE: geeks for geeks article on "SHA-256 Hash in Java"
+    /** REFERENCE: geeks for geeks article on "SHA-256 Hash in Java"
+     @param hash sha-256 encryption of input message, byte array
+     @return hexString hexadecimal string of SHA-256 hash
+     */
     public static String toHexidecimalString(byte[] hash)
     {
         // Convert byte array into sign number representation
@@ -28,6 +34,11 @@ public class ToHash {
     }
 
     // through sha256 with salt and out as a hash product
+    /** adds a salt to the input message and gets it's SHA-256 hash
+     @param message string that is to be encrypted with SHA-256
+     @return SHA256 hash of the message in a string
+     @throws NoSuchAlgorithmException if sha-256 algorithm can't be found
+     */
     public static String intoHash(String message) throws NoSuchAlgorithmException {
         message += salt;
         MessageDigest md = MessageDigest.getInstance("SHA-256");
