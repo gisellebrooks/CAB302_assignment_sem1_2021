42dd1389ec75167701722399cbf2eb36f4a437ba
Alexandra Pereira <ali20_4@hotmail.com>
2021-04-26 14:00:31 +1000

    Login barebones

--- MarketPlace/src/GUI/GUI.java
+++ MarketPlace/src/GUI/GUI.java
@@ -1,15 +1,74 @@
 package GUI;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
+
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel passwordLabel;
+    private static JTextField passwordText;
+    private static JButton button;
+    private static JLabel valid;
+    private static JLabel invalid;
+
     public static void main(String[] args) {
-
+        JPanel panel = new JPanel();
         JFrame frame = new JFrame();
-        frame.setSize(100,100);
+        frame.setSize(350,200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.add(panel);
+        panel.setLayout(null);
+
+        userLabel = new JLabel("User");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        userText = new JTextField(20);
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
+
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+
+        passwordText = new JPasswordField();
+        passwordText.setBounds(100, 50, 160, 25);
+        panel.add(passwordText);
+
+        button = new JButton("Login");
+        button.setBounds(10, 80, 80, 25);
+        button.addActionListener(new GUI());
+        panel.add(button);
+
+        valid = new JLabel("");
+        valid.setForeground(Color.green);
+        valid.setBounds(10, 110, 300, 25);
+        panel.add(valid);
+
+        invalid = new JLabel("");
+        invalid.setForeground(Color.red);
+        invalid.setBounds(10, 110, 300, 25);
+        panel.add(invalid);
+
+
         frame.setVisible(true);
 
-        JPanel panel = new JPanel();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      String user = userText.getText();
+      String password = passwordText.getText();
+      valid.setText("");
+      invalid.setText("");
+      if(user.equals("Alex") && password.equals("fluffyDino123")){
+          valid.setText("Login successful!");
+      } else {
+          invalid.setText("Invalid user name or password");
+      }
     }
 }
