af874abee6c1849b044c24c633bf96d75e750399
Alexandra Pereira <ali20_4@hotmail.com>
2021-04-26 13:29:51 +1000

    Add barebones GUI

--- MarketPlace/src/GUI/GUI.java
+++ MarketPlace/src/GUI/GUI.java
@@ -1,2 +1,15 @@
-package GUI;public class GUI {
+package GUI;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setSize(100,100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();
+    }
 }
