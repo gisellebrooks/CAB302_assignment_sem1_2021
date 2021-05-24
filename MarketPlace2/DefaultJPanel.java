11985c499cec41d2c6ff73027767f478e5c10752
Alexandra Pereira <ali20_4@hotmail.com>
2021-05-24 20:45:55 +1000

    make a standardised jpanel

---
+++ MarketPlace2/DefaultJPanel.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class DefaultJPanel extends JPanel {
+
+    public DefaultJPanel() {
+        super();
+        setBackground(Color.WHITE);
+    }
+
+}
